6cc2537f611a8eb79567e7e6c2220e9a7c866e77
Damon E. Drake <damon.e.drake@gmail.com>
2016-09-26 13:40:56 -0500

    Adding initial code before moving to dev branch and cleaning up this rough draft.

---
+++ Mongo.Repository/MongoCollectionSet.cs
@@ -0,0 +1,69 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Mongo.Repository {
+  public class MongoCollectionSet<T> : IEnumerable<T> {
+    public IMongoCollection<T> Collection { get; private set; }
+
+    public long TotalDocuments {
+      get { return Collection.Count(new BsonDocument()); }
+    }
+
+    public MongoCollectionSet(MongoClient client, string Database) {
+      Collection = client.GetDatabase(Database).GetCollection<T>(GetCollectionName());
+    }
+
+    public void Add(T obj) {
+      Collection.InsertOne(obj);
+    }
+
+    public void Update(string ID, T obj) {
+      Collection.ReplaceOne(new BsonDocument("_id", new BsonObjectId(new ObjectId(ID))), obj);
+    }
+
+    public void Remove(string ID) {
+      Collection.DeleteOne(new BsonDocument("_id", new BsonObjectId(new ObjectId(ID))));
+    }
+
+    public T Find(string ID) {
+      var id = new BsonObjectId(new ObjectId(ID));
+      return Collection.Find(new BsonDocument("_id", id)).FirstOrDefault();
+    }
+
+    public IEnumerable<T> Where(Expression<Func<T, bool>> expr) {
+      return Collection.AsQueryable().Where(expr).ToList();
+    }
+
+    public IEnumerable<T> Select(Expression<Func<T, T>> expr) {
+      return Collection.AsQueryable().Select(expr);
+    }
+
+    public T FirstOrDefault(Expression<Func<T, bool>> expr) {
+      return Collection.AsQueryable().FirstOrDefault(expr);
+    }
+
+    public async Task<IEnumerable<T>> ToListAsync() {
+      return await Collection.AsQueryable().ToListAsync();
+    }
+
+    private string GetCollectionName() {
+      var attr = typeof(T).GetCustomAttributes(typeof(CollectionNameAttribute), true).FirstOrDefault() as CollectionNameAttribute;
+      if (attr != null) { return attr.Name; }
+
+      return typeof(T).Name.ToLower();
+    }
+
+    public IEnumerator<T> GetEnumerator() {
+      return Collection.AsQueryable().ToList().GetEnumerator();
+    }
+    IEnumerator IEnumerable.GetEnumerator() {
+      return GetEnumerator();
+    }
+  }
+}
---
+++ Mongo.Repository/MongoDBContext.cs
@@ -0,0 +1,55 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+
+namespace Mongo.Repository {
+  [AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
+  public class CollectionNameAttribute : Attribute {
+    public string Name { get; private set; }
+    public CollectionNameAttribute(string name) {
+      Name = name;
+    }
+  }
+  public class MongoDBContext : IDisposable {
+    private static List<IMongoClient> Clients = new List<IMongoClient>();
+    private static List<string> Contexts = new List<string>();
+    private static List<string> Databases = new List<string>();
+
+    public MongoDBContext(string Name) {
+      if (!Contexts.Contains(Name)) {
+        var cn = ConfigurationManager.ConnectionStrings[Name].ConnectionString;
+        var url = new MongoUrl(cn);
+
+        Contexts.Add(Name);
+        Clients.Add(new MongoClient(cn));
+        Databases.Add(url.DatabaseName);
+      }
+
+      var index = Contexts.FindIndex(x => x == Name);
+
+      foreach (var prop in GetType().GetProperties()) {
+        var t = prop.PropertyType;
+        if (t.ToString().Contains("MongoCollectionSet")) {
+          var instance = Activator.CreateInstance(t, new object[] { Clients[index], Databases[index] });
+          prop.SetValue(this, instance);
+        }
+      }
+    }
+
+    private string GetCollectionName<T>() {
+      var attr = typeof(T).GetCustomAttributes(typeof(CollectionNameAttribute), true).FirstOrDefault() as CollectionNameAttribute;
+      if (attr != null) { return attr.Name; }
+
+      return typeof(T).Name.ToLower();
+    }
+
+    public void Dispose() {
+      foreach (var prop in GetType().GetProperties()) {
+        prop.SetValue(this, null);
+      }
+    }
+  }
+  
+}
