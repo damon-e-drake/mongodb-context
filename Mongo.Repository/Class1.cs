fc3697a54978f2c93595eefd0e1c4ae11364d0ed
Damon E. Drake <damon.e.drake@gmail.com>
2016-09-26 13:21:40 -0500

    Starting Initial Project

--- Mongo.Repository/Class1.cs
+++ Mongo.Repository/Class1.cs
@@ -3,12 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Mongo.Repository
-{
-    public class Class1
-    {
-        public Class1()
-        {
-        }
+namespace Mongo.Repository {
+  public class Class1 {
+    public Class1() {
     }
+  }
 }
