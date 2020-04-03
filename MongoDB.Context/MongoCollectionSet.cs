3609345a310c24ed38085bb4a2c060cd9cf4db9b
Damon Drake <damon.drake@evergy.com>
2020-04-02 23:25:39 -0500

    Implementing extracted interface.

--- MongoDB.Context/MongoCollectionSet.cs
+++ MongoDB.Context/MongoCollectionSet.cs
@@ -1,5 +1,6 @@
 ﻿using MongoDB.Bson;
 using MongoDB.Context.Attributes;
+using MongoDB.Context.Interfaces;
 using MongoDB.Driver;
 using System;
 using System.Collections;
@@ -9,62 +10,76 @@ using System.Linq.Expressions;
 using System.Threading.Tasks;
 
 namespace MongoDB.Context {
-  public class MongoCollectionSet<T> : IEnumerable<T> {
+  public class MongoCollectionSet<T> : IMongoCollectionSet<T> where T : class
+  {
     public IMongoCollection<T> Collection { get; private set; }
 
-    public long TotalDocuments {
+    public long TotalDocuments
+    {
       get { return Collection.CountDocuments(FilterDefinition<T>.Empty); }
     }
 
-    public MongoCollectionSet(IMongoDatabase database) {
+    public MongoCollectionSet(IMongoDatabase database)
+    {
       Collection = database.GetCollection<T>(GetCollectionName());
     }
 
-    public void Add(T obj) {
+    public void Add(T obj)
+    {
       Collection.InsertOne(obj);
     }
 
-    public void Update(string ID, T obj) {
+    public void Update(string ID, T obj)
+    {
       Collection.ReplaceOne(new BsonDocument("_id", new BsonObjectId(new ObjectId(ID))), obj);
     }
 
-    public void Remove(string ID) {
+    public void Remove(string ID)
+    {
       Collection.DeleteOne(new BsonDocument("_id", new BsonObjectId(new ObjectId(ID))));
     }
 
-    public T Find(string ID) {
+    public T Find(string ID)
+    {
       var id = new BsonObjectId(new ObjectId(ID));
       return Collection.Find(new BsonDocument("_id", id)).FirstOrDefault();
     }
 
-    public IEnumerable<T> Where(Expression<Func<T, bool>> expr) {
+    public IEnumerable<T> Where(Expression<Func<T, bool>> expr)
+    {
       return Collection.AsQueryable().Where(expr).ToList();
     }
 
-    public IEnumerable<T> Select(Expression<Func<T, T>> expr) {
+    public IEnumerable<T> Select(Expression<Func<T, T>> expr)
+    {
       return Collection.AsQueryable().Select(expr);
     }
 
-    public T FirstOrDefault(Expression<Func<T, bool>> expr) {
+    public T FirstOrDefault(Expression<Func<T, bool>> expr)
+    {
       return Collection.AsQueryable().FirstOrDefault(expr);
     }
 
-    public async Task<IEnumerable<T>> ToListAsync() {
+    public async Task<IEnumerable<T>> ToListAsync()
+    {
       return await Collection.AsQueryable().ToListAsync();
     }
 
-    private string GetCollectionName() {
+    private string GetCollectionName()
+    {
       var attr = typeof(T).GetCustomAttributes(typeof(CollectionNameAttribute), true).FirstOrDefault() as CollectionNameAttribute;
       if (attr != null) { return attr.Name; }
 
       return typeof(T).Name.ToLower();
     }
 
-    public IEnumerator<T> GetEnumerator() {
+    public IEnumerator<T> GetEnumerator()
+    {
       return Collection.AsQueryable().ToList().GetEnumerator();
     }
 
-    IEnumerator IEnumerable.GetEnumerator() {
+    IEnumerator IEnumerable.GetEnumerator()
+    {
       return GetEnumerator();
     }
   }
