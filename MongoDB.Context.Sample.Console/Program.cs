7d0084023b99d1a1aa68138f861fc168693dbc50
Damon E. Drake <damon.e.drake@gmail.com>
2020-04-04 12:04:48 -0500

    Removed analyzer warnings for literal.

--- MongoDB.Context.Sample.Console/Program.cs
+++ MongoDB.Context.Sample.Console/Program.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Diagnostics.CodeAnalysis;
-using System.Threading.Tasks;
-
-namespace MongoDB.Context.Sample
+﻿namespace MongoDB.Context.Sample
 {
   public static class Program
   {
-    [SuppressMessage("Globalization", "CA1303:Do not pass literals as localized parameters", Justification = "A method test.")]
-    public static async Task Main(string[] args)
+    public static void Main()
     {
-      Console.WriteLine("Hello World!");
+
     }
   }
 }
